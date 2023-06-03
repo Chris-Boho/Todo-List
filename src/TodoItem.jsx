d5d8b358aae1df0fdde6b9826fe2637e613ca262
Chris B <chrisishere4@gmail.com>
2023-06-03 00:10:48 -0400

    format changes

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -2,12 +2,16 @@ export function TodoItem({ completed, id, title, toggleTodo, deleteTodos }) {
     return (
         <li>
             <label>
-                <input type="checkbox" checked={completed}
-                    onChange={e => toggleTodo(id, e.target.checked)} />
+                <input
+                    type="checkbox"
+                    checked={completed}
+                    onChange={e => toggleTodo(id, e.target.checked)}
+                />
                 {title}
             </label>
-            <button onClick={() => deleteTodos(id)}
-                className="btn btn-danger">Delete</button>
+            <button onClick={() => deleteTodos(id)} className="btn btn-danger">
+                Delete
+            </button>
         </li>
     )
 }
